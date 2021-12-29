41334bbd07536e83465e11302c2df153ba27899d
Boris <boristopalov1@gmail.com>
2021-12-28 23:54:33 -0500

    set up express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import config from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => { 
     // connect to database
@@ -11,10 +12,19 @@ const main = async () => {
     orm.getMigrator().up();
 
     // creates an instance of post, doesn't add it to the database yet
-    const post = orm.em.create(Post, {title: 'My first post'});
-
+    // const post = orm.em.create(Post, {title: 'My first post'});
     // adds post to the database
-    await orm.em.persistAndFlush(post); 
+    // await orm.em.persistAndFlush(post); 
+
+    const app = express();
+
+    app.get("/", (_, res) => { 
+        res.send("hello")
+    })
+
+    app.listen(8080, () => { 
+        console.log("listening on port 8080")
+    })
 
     // console.log(await orm.em.find(Post, {}))
 };
