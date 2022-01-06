ea68281d926d6eef02c2be41a605161ac8335937
Boris <boristopalov1@gmail.com>
2022-01-06 18:27:13 -0500

    finish crud for post entity

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,17 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
-
+  @Field( () => Int )
   @PrimaryKey()
   id!: number;
 
+  @Field()
   @Property()
   title!: string;
 
+  @Field()
   @Property()
   createdAt: Date = new Date();
 
+  @Field()
   @Property({ onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,20 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import "reflect-metadata";
 // import { Post } from "./entities/Post";
 import config from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => { 
     // connect to database
     const orm = await MikroORM.init(config);
 
     // run migrations
-    orm.getMigrator().up();
+    await orm.getMigrator().up();
 
     // creates an instance of post, doesn't add it to the database yet
     // const post = orm.em.create(Post, {title: 'My first post'});
@@ -17,10 +22,20 @@ const main = async () => {
     // await orm.em.persistAndFlush(post); 
 
     const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [
+                HelloResolver, PostResolver
+            ],
+            validate: false,
+            
+        }),
+        context: () => ({em: orm.em})
+    });
 
-    app.get("/", (_, res) => { 
-        res.send("hello")
-    })
+    await apolloServer.start();
+    // creates a graphql endpoint
+    apolloServer.applyMiddleware({ app })
 
     app.listen(8080, () => { 
         console.log("listening on port 8080")
